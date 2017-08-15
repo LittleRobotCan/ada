c1bd5395bf2fd87cbc32b3cde8e36593dc70b75b
RuiweiJiang <ruiwei06@gmail.com>
2017-08-14 22:43:28 -0700

    2nd learner added

--- classifier_module/classifiers.py
+++ classifier_module/classifiers.py
@@ -1,10 +1,8 @@
-from sklearn.svm import SVC
-from sklearn import svm
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier
 from sklearn.svm import SVC
-from sklearn.model_selection import KFold, train_test_split
-from math import sqrt
+from sklearn.model_selection import KFold
+import xgboost as xgb
 import pandas as pd
 
 def prep_matrix(data):
@@ -39,10 +37,24 @@ class SKlearnHelper(object):
     def feature_importances(self, x, y):
         print(self.clf.fit(x,y).feature_importances_)
 
+
+def split_by_era(eras, n_splits):
+    era_set = list(set(eras))
+    splits = []
+    kf = KFold(n_splits=n_splits, shuffle=True)
+    for _, test_index in kf.split(era_set):
+        test_era = [era_set[i] for i in test_index]
+        train_era = [era for era in era_set if era not in test_era]
+        test_index = [i for i in range(len(eras)) if eras[i] in test_era]
+        train_index = [i for i in range(len(eras)) if eras[i] in train_era]
+        splits.append({'test':test_index, 'train':train_index})
+    return splits
+
+
 # Put in our parameters for said classifiers
 # Random Forest parameters
-class stacker():
-    def __init__(self, data, test_size):
+class base_learner():
+    def __init__(self):
         rf_params = {
             'n_jobs': -1,
             'n_estimators': 500,
@@ -92,32 +104,16 @@ class stacker():
         self.gb = SKlearnHelper(clf=GradientBoostingClassifier, seed=SEED, params=gb_params)
         self.svc = SKlearnHelper(clf=SVC, seed=SEED, params=svc_params)
 
-        # get the era value for splitting
-        self.era = data['era'].tolist()
-        self.n_splits = 10
-
-    def split_by_era(self):
-        era_set = list(set(self.era))
-        splits = []
-        kf = KFold(n_splits=self.n_splits, shuffle=True)
-        for _, test_index in kf.split(era_set):
-            test_era = [era_set[i] for i in test_index]
-            train_era = [era for era in era_set if era not in test_era]
-            test_index = [i for i in range(len(self.era)) if self.era[i] in test_era]
-            train_index = [i for i in range(len(self.era)) if self.era[i] in train_era]
-            splits.append({'test':test_index, 'train':train_index})
-        return splits
-
-    def stacking(self, X, y, X_holdout, y_holdout):
-        n_splits = 5
+    def stacking(self, X, y, X_holdout, y_holdout, splits):
         base_models = [self.rf, self.et, self.ada, self.gb, self.svc]
         stacks = []
-        splits = self.split_by_era()
         holdout_matrix = []
         for model in base_models:
+            print "model"
             X_stack = np.zeros((len(X)))
             holdout_col = []
             for index in splits:
+                print "split"
                 X_train, X_test = X[index['train']], X[index['test']]
                 y_train = y[index['train']]
                 model.fit(X_train, y_train)
@@ -125,18 +121,19 @@ class stacker():
                 holdout_col.extend(model.predict(X_holdout))
             stacks.append(X_stack)
             holdout_matrix.append(holdout_col)
-        stacks_data = np.array(X_stack).transpose()
+        stacks_data = np.array(stacks).transpose()
         stacks_df = pd.DataFrame(stacks_data, columns = ['rf', 'et', 'ada', 'gb', 'svc'])
         stacks_df['labels'] = y
         holdout_data = np.array(holdout_matrix).transpose()
         holdout_df = pd.DataFrame(holdout_data, columns = ['rf', 'et', 'ada', 'gb', 'svc'])
-        holdout_df['labels'] = y_holdout*n_splits
+        holdout_df['labels'] = list(y_holdout)*len(splits)
         return stacks_df, holdout_df
 
 
-def second_learning(self, stacks_df):
+def top_learner(stacks_df):
     X, y = prep_matrix(stacks_df)
-    gbm = xgb.XGBClassifier(        # learning_rate = 0.02,
+    gbm = xgb.XGBClassifier(
+        # learning_rate = 0.02,
         n_estimators=2000,
         max_depth=4,
         min_child_weight=2,
@@ -145,17 +142,25 @@ def second_learning(self, stacks_df):
         subsample=0.8,
         colsample_bytree=0.8,
         objective='binary:logistic',
-        nthread=-1,
+        n_jobs=-1,
         scale_pos_weight=1).fit(X, y)
     return gbm
 
 
 if __name__ == '__main__':
     from pandas import read_csv
-    data = read_csv('data/numerai_training_data.csv')
+    raw_data = read_csv('data/numerai_training_data.csv')
+    data = raw_data.sample(20)
     t_data = read_csv('data/numerai_tournament_data.csv')
-    holdout = t_data[t_data['data_type']=='validation']
+    holdout_raw = t_data[t_data['data_type']=='validation']
+    holdout = holdout_raw.sample(20)
     X, y = prep_matrix(data.ix[:,3:])
     X_holdout, y_holdout = prep_matrix(holdout.ix[:,3:])
-    stack = stacker()
-    stacks_df, holdout_df = stack.stacking(X, y, X_holdout, y_holdout)
+    eras = data['era'].tolist()
+    splits = split_by_era(eras, 2)
+    stack = base_learner()
+    stacks_df, holdout_df = stack.stacking(X, y, X_holdout, y_holdout, splits)
+    gbm = top_learner(stacks_df)
+    X_test, y_test = prep_matrix(holdout_df)
+    predictions = gbm.predict(X_test)
+    print "finished"
